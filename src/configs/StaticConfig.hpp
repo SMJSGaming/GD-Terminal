5aaf1a4ffb186f8969633daf54afd20640cf684f
SMJSGaming <sjoerd.t@live.nl>
2022-10-10 00:59:05 +0200

    Added minor text changes and standardized padding

--- src/configs/StaticConfig.hpp
+++ src/configs/StaticConfig.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 const float CHAR_SCALE = 0.4f;
+const float PADDING = 5;
 const char* FONT = "chatFont.fnt";
-const char* START_TEXT = R"(
-Geometry Dash terminal edition [version 0.1.0]
-Use the help command or use the -h flag for more information
-)";
+const char* START_TEXT = R"(Geometry Dash terminal edition [version 0.1.0]
+Use the help command or the -h flag for more information.)";
