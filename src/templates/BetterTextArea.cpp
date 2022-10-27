c687d511612992cd0767d51fb8f1e64e19ece0b8
SMJSGaming <sjoerd.t@live.nl>
2022-10-27 21:38:54 +0200

    Simplified better text area

---
+++ src/templates/BetterTextArea.cpp
@@ -0,0 +1,105 @@
+#include "BetterTextArea.hpp"
+
+BetterTextArea* gd::BetterTextArea::create(const char* font, const char* text, float scale) {
+    BetterTextArea* instance = new BetterTextArea(font, text, scale);
+
+    if (instance && instance->init()) {
+        instance->autorelease();
+
+        return instance;
+    } else {
+        CC_SAFE_DELETE(instance);
+
+        return nullptr;
+    }
+}
+
+gd::BetterTextArea::BetterTextArea(const char* font, const char* text, float scale) {
+    this->m_font = font;
+    this->m_text = text;
+    this->m_scale = scale;
+
+    this->updateContents();
+}
+
+std::vector<gd::MonoSpaceLabel*> gd::BetterTextArea::getLines() {
+    return this->m_lines;
+}
+
+CCSize gd::BetterTextArea::getCharSize(bool scale) {
+    return MonoSpaceLabel::create("", this->m_font, this->m_scale)->getCharSize(scale);
+}
+
+void gd::BetterTextArea::setFont(const char* font) {
+    this->m_font = font;
+
+    this->updateContents();
+}
+
+const char* gd::BetterTextArea::getFont() {
+    return this->m_font;
+}
+
+void gd::BetterTextArea::setString(const char* text) {
+    this->m_text = text;
+
+    this->updateContents();
+}
+
+const char* gd::BetterTextArea::getString() {
+    return this->m_text;
+}
+
+void gd::BetterTextArea::setScale(float scale) {
+    this->m_scale = scale;
+
+    this->updateContents();
+}
+
+float gd::BetterTextArea::getScale() {
+    return this->m_scale;
+}
+
+void gd::BetterTextArea::setLinePadding(float padding) {
+    this->m_linePadding = padding;
+
+    this->updateContents();
+}
+
+float gd::BetterTextArea::getLinePadding() {
+    return this->m_linePadding;
+}
+
+void gd::BetterTextArea::updateContents() {
+    CCSize charSize = this->getCharSize();
+    std::stringstream stream(this->m_text);
+    std::string line;
+
+    this->m_lines.clear();
+
+    while (std::getline(stream, line)) {
+        MonoSpaceLabel* label = MonoSpaceLabel::create(line.c_str(), this->m_font, this->m_scale);
+
+        label->setAnchorPoint({ 0, 0.5f });
+        
+        this->m_lines.push_back(label);
+    }
+
+    const unsigned int lineCount = this->m_lines.size();
+
+    this->removeAllChildren();
+    this->setContentSize({ 
+        (*std::max_element(this->m_lines.begin(), this->m_lines.end(), [](MonoSpaceLabel* a, MonoSpaceLabel* b) {
+            return a->getContentSize().width < b->getContentSize().width;
+        }))->getContentSize().width * this->m_scale,
+        charSize.height * lineCount + this->m_linePadding * (lineCount - 1)
+    });
+
+    for (unsigned int i = 0; i < lineCount; i++) {
+        MonoSpaceLabel* label = this->m_lines.at(i);
+
+        label->setPosition({ 0, this->getContentSize().height - charSize.height * i });
+
+        this->addChild(label);
+    }
+}
--- src/templates/BetterTextArea.hpp
+++ src/templates/BetterTextArea.hpp
@@ -2,177 +2,28 @@
 
 #include "../includes.hpp"
 
-template<bool TTF>
-using CCLabel = std::conditional_t<TTF, CCLabelTTF, CCLabelBMFont>;
-
-template<bool TTF>
-std::vector<CCLabel<TTF>*> _getLines(std::string font, std::string text, float scale, float padding) {
-    std::vector<CCLabel<TTF>*> lines;
-    std::stringstream stream(text);
-    std::string line;
-
-    for (float top = 0; std::getline(stream, line); top -= padding) {
-        CCLabel<TTF>* label;
-
-        if constexpr (TTF) {
-            label = CCLabelTTF::create(line.c_str(), font.c_str(), scale);
-        } else {
-            label = CCLabelBMFont::create(line.c_str(), font.c_str());
-
-            label->setScale(scale);
-        }
-
-        label->setAnchorPoint({ 0, 0.5f });
-        label->setPosition({ 0, top });
-        lines.push_back(label);
-
-        top -= label->getContentSize().height * scale;
-    }
-
-    return lines;
-}
-
-template<bool TTF>
-float _maxLineWidth(std::vector<CCLabel<TTF>*> lines) {
-    return (*std::max_element(lines.begin(), lines.end(), [](CCLabel<TTF>* a, CCLabel<TTF>* b) {
-        return a->getContentSize().width < b->getContentSize().width;
-    }))->getContentSize().width;
-}
-
 namespace gd {
-    template<bool TTF>
     struct BetterTextArea : public CCNode {
+        static BetterTextArea* create(const char* font, const char* text, float scale = 1);
 
-        static BetterTextArea* create(std::string font, std::string text, float scale) {
-            std::vector<CCLabel<TTF>*> lines = _getLines<TTF>(font, text, scale, 0);
-
-            return BetterTextArea::create<TTF>(font, text, lines, scale, _maxLineWidth<TTF>(lines), false, false);
-        }
-
-        static BetterTextArea* create(std::string font, std::string text, float scale, float width) {
-            return BetterTextArea::create<TTF>(font, text, _getLines<TTF>(font, text, scale, 0), scale, width, true);
-        }
-
-        void setFont(std::string font) {
-            this->m_font = font;
-
-            this->updateContents();
-        }
-
-        std::string getFont() {
-            return this->m_font;
-        }
-
-        void setText(std::string text) {
-            this->m_text = text;
-
-            this->updateContents();
-        }
-
-        std::string getText() {
-            return this->m_text;
-        }
-
-        std::vector<CCLabel<TTF>*> getLines() {
-            return this->m_lines;
-        }
-
-        void setWidth(float width) {
-            this->m_artificialWidth = true;
-
-            this->setContentSize({ width, this->getContentSize().height });
-        }
-
-        float getWidth() {
-            return this->getContentSize().width;
-        }
-
-        float getHeight() {
-            return this->getContentSize().height;
-        }
-
-        void setScale(float scale) {
-            this->m_scale = scale;
-
-            this->updateContents();
-        }
-
-        float getScale() {
-            return this->m_scale;
-        }
-
-        float getLineHeight() {
-            return this->m_lineHeight;
-        }
-
-        void setLinePadding(float padding) {
-            this->m_linePadding = padding;
-
-            this->updateContents();
-        }
-
-        float getLinePadding() {
-            return this->m_linePadding;
-        }
-
+        std::vector<MonoSpaceLabel*> getLines();
+        CCSize getCharSize(bool scale = true);
+        void setFont(const char* font);
+        const char* getFont();
+        void setString(const char* text);
+        const char* getString();
+        void setScale(float scale);
+        float getScale();
+        void setLinePadding(float padding);
+        float getLinePadding();
     private:
-        template<bool TTF>
-        static BetterTextArea* create(std::string font, std::string text, std::vector<CCLabel<TTF>*> lines, float scale, float width, bool artificialWidth) {
-            BetterTextArea* textArea = new BetterTextArea<TTF>(font, text, lines, scale, width, artificialWidth);
-
-            if (textArea) {
-                textArea->autorelease();
-
-                return textArea;
-            } else {
-                CC_SAFE_DELETE(textArea);
-                
-                return nullptr;
-            }
-        }
-
-        std::string m_font;
-        std::string m_text;
-        std::vector<CCLabel<TTF>*> m_lines;
+        std::vector<MonoSpaceLabel*> m_lines;
+        const char* m_font;
+        const char* m_text;
         float m_scale;
-        float m_lineHeight;
         float m_linePadding;
-        bool m_artificialWidth;
 
-        BetterTextArea(std::string font, std::string text, std::vector<CCLabel<TTF>*> lines, float scale, float width, bool artificialWidth) {
-            const unsigned int lineCount = lines.size();
-
-            this->m_font = font;
-            this->m_text = text;
-            this->m_lines = lines;
-            this->m_scale = scale;
-            this->m_lineHeight = lines.back()->getContentSize().height * scale;
-            this->m_linePadding = 0;
-            this->m_artificialWidth = artificialWidth;
-
-            this->setContentSize({ width, this->m_lineHeight * lineCount + this->m_linePadding * (lineCount - 1) });
-
-            for (unsigned int i = 0; i < lineCount; i++) {
-                this->addChild(this->m_lines.at(i));
-            }
-        }
-
-        void updateContents() {
-            this->m_lines = _getLines<TTF>(this->m_font, this->m_text, this->m_scale, this->m_linePadding);
-            this->m_lineHeight = this->m_lines.back()->getContentSize().height * this->m_scale;
-
-            const unsigned int lineCount = this->m_lines.size();
-
-            this->setContentSize({
-                this->m_artificialWidth ? this->getWidth(): _maxLineWidth<TTF>(this->m_lines),
-                this->m_lineHeight * lineCount + this->m_linePadding * (lineCount - 1)
-            });
-
-            this->removeAllChildren();
-
-            for (unsigned int i = 0; i < lineCount; i++) {
-                this->addChild(this->m_lines.at(i));
-            }
-        }
+        BetterTextArea(const char* font, const char* text, float scale);
+        void updateContents();
     };
 }
