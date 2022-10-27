481f10b4875152805d133f3a10124f5b77fa2beb
SMJSGaming <sjoerd.t@live.nl>
2022-10-27 12:50:32 +0200

    Added consola fnt files

--- src/configs/StaticConfig.hpp
+++ src/configs/StaticConfig.hpp
@@ -7,6 +7,6 @@
 #define CURSOR 0x92B846A
 #define CHAR_SCALE 0.4f
 #define PADDING 5.0f
-#define FONT "chatFont.fnt"
+#define FONT "consola.fnt"
 #define START_TEXT "Geometry Dash terminal edition [version 0.1.0]\n\
 Use the help command or the -h flag for more information.\n "
