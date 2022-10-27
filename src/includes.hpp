31550e43df1de99f05a0b371f5a23f8e6ebac9ba
SMJSGaming <sjoerd.t@live.nl>
2022-10-27 12:58:39 +0200

    Added several default includes

--- src/includes.hpp
+++ src/includes.hpp
@@ -4,4 +4,9 @@
 #define CONCAT_LITERAL(a, b) _CONCAT_LITERAL(a, b)
 
 #include "templates/gd_extra.hpp"
+#include "configs/StaticConfig.hpp"
+#include "templates/BetterTextArea.hpp"
+#include "templates/CursorNode.hpp"
+#include "templates/MonoSpaceLabel.hpp"
+#include "templates/Command.hpp"
 #include "registers/Hook.hpp"
