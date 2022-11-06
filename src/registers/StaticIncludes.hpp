8af1dbb4f2551273bf5f6e2f6c17691554a2e775
SMJSGaming <sjoerd.t@live.nl>
2022-11-06 20:39:31 +0100

    Removed some useless static includes

--- src/registers/StaticIncludes.hpp
+++ src/registers/StaticIncludes.hpp
@@ -1,10 +1,4 @@
 #pragma once
 
-// Hooks
 #include "../hooks/MenuLayer.hpp"
 #include "../hooks/CCEGLView.hpp"
-
-// Commands
-#include "../commands/Echo.hpp"
-#include "../commands/Quit.hpp"
-#include "../commands/Help.hpp"
